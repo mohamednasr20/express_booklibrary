61dd3e9040a6fe646f4cda8e610f82b7ce127e16
mohamednasr20 <mohamednasr8687@gmail.com>
2021-02-21 23:59:46 -0500

    create book detail page

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -45,32 +45,53 @@ exports.book_list = function (req, res, next) {
     });
 };
 
-// Display detail page for a specific book.
-exports.book_detail = (req, res) => {
-  res.send('NOT IMPLEMENTED: Book detail: ' + req.params.id);
+exports.book_detail = (req, res, next) => {
+  async.parallel(
+    {
+      book: (callback) => {
+        Book.findById(req.params.id)
+          .populate('author')
+          .populate('genre')
+          .exec(callback);
+      },
+      book_instance: (callback) => {
+        BookInstance.find({ book: req.params.id }).exec(callback);
+      },
+    },
+    (err, results) => {
+      if (err) {
+        return next(err);
+      }
+      if (results.book == null) {
+        let err = new Error('Book not found');
+        err.status = 404;
+        return next(err);
+      }
+      res.render('book_detail', {
+        title: results.book.title,
+        book: results.book,
+        book_instances: results.book_instance,
+      });
+    }
+  );
 };
 
-// Display book create form on GET.
 exports.book_create_get = (req, res) => {
   res.send('NOT IMPLEMENTED: Book create GET');
 };
 
-// Handle book create on POST.
 exports.book_create_post = (req, res) => {
   res.send('NOT IMPLEMENTED: Book create POST');
 };
 
-// Display book delete form on GET.
 exports.book_delete_get = (req, res) => {
   res.send('NOT IMPLEMENTED: Book delete GET');
 };
 
-// Handle book delete on POST.
 exports.book_delete_post = (req, res) => {
   res.send('NOT IMPLEMENTED: Book delete POST');
 };
 
-// Display book update form on GET.
 exports.book_update_get = (req, res) => {
   res.send('NOT IMPLEMENTED: Book update GET');
 };
