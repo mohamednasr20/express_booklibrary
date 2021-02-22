81080e49dbaab642286eb3312deb3ad2cc31d798
mohamednasr20 <mohamednasr8687@gmail.com>
2021-02-22 00:04:45 -0500

    create author detail page

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,4 +1,6 @@
 const Author = require('../models/author');
+const async = require('async');
+const Book = require('../models/book');
 
 exports.author_list = function (req, res, next) {
   Author.find()
@@ -14,37 +16,54 @@ exports.author_list = function (req, res, next) {
     });
 };
 
-// Display detail page for a specific Author.
-exports.author_detail = (req, res) => {
-  res.send(`NOT IMPLEMENTED: Author detail:${req.params.id}`);
+exports.author_detail = (req, res, next) => {
+  async.parallel(
+    {
+      author: (callback) => {
+        Author.findById(req.params.id).exec(callback);
+      },
+      authors_books: (callback) => {
+        Book.find({ author: req.params.id }, 'title summary').exec(callback);
+      },
+    },
+    (err, results) => {
+      if (err) {
+        return next(err);
+      }
+      if (results.author == null) {
+        var err = new Error('Author not found');
+        err.status = 404;
+        return next(err);
+      }
+      res.render('author_detail', {
+        title: 'Author Detail',
+        author: results.author,
+        author_books: results.authors_books,
+      });
+    }
+  );
 };
 
-// Display Author create form on GET.
 exports.author_create_get = (req, res) => {
   res.send('NOT IMPLEMENTED: Author create GET');
 };
 
-// Handle Author create on POST.
 exports.author_create_post = (req, res) => {
   res.send('NOT IMPLEMENTED: Author create POST');
 };
 
-// Display Author delete form on GET.
 exports.author_delete_get = (req, res) => {
   res.send('NOT IMPLEMENTED: Author delete GET');
 };
 
-// Handle Author delete on POST.
 exports.author_delete_post = (req, res) => {
   res.send('NOT IMPLEMENTED: Author delete POST');
 };
 
-// Display Author update form on GET.
 exports.author_update_get = (req, res) => {
   res.send('NOT IMPLEMENTED: Author update GET');
 };
 
-// Handle Author update on POST.
 exports.author_update_post = (req, res) => {
   res.send('NOT IMPLEMENTED: Author update POST');
 };
